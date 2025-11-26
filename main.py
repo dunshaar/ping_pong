0e12392e254079f0a5f949384fe9bd38ffe4aaa0
Daniil S. <daniils.@MacBook-Air-Daniil.local>
2025-11-26 17:52:45 +0300

    add new class GameSprite

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from pygame import *
 
 window = display.set_mode((900, 800))
 
-clock = time.Clock()
+class GameSprite(sprite.Sprite):
+    def __init__(self, filename, x, y, width=30, height=200):
+        super().__init__()
+        self.image = transform.scale(image.load(filename), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
+clock = time.Clock()
 game = True
 
 while game:
