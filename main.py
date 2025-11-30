9fb59214d3b532363d43556a84cc34f675ef474f
Daniil S. <daniils.@MacBook-Air-Daniil.local>
2025-11-30 16:42:42 +0300

    added sprites

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ class GameSprite(sprite.Sprite):
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+platform_left = GameSprite("player-left.png", 50, 350)
+platform_right = GameSprite("player-right.png", 800, 350)
+ball = GameSprite("pball.png", 400, 350, width=100, height=100)
+
 clock = time.Clock()
 game = True
 
@@ -20,5 +24,17 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    pressed = key.get_pressed()
+    if pressed[K_UP] and platform_right.rect.y >= 0:
+        platform_right.rect.y -= 10
+    if pressed[K_DOWN] and platform_right.rect.y <= 800 - platform_right.rect.height:
+        platform_right.rect.y += 10
+    if pressed[K_w] and platform_left.rect.y >= 0:
+        platform_left.rect.y -= 10
+    if pressed[K_s] and platform_left.rect.y <= 800 - platform_left.rect.height:
+        platform_left.rect.y += 10
+    platform_left.draw()
+    platform_right.draw()
+    ball.draw()
     display.update()
     clock.tick(60)
